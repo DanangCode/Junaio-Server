bbdc17463fd9d9186e988ef0d26e9b338d64b80f
Jasper Gregory <jaspergregory@gmail.com>
2011-01-09 19:16:10 -0800

    .

--- src/event.php
+++ src/event.php
@@ -18,22 +18,40 @@
  * 
  */
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-<results>
-   <poi id=\"1\" interactionfeedback=\"none\">
-    <name><![CDATA[The Erotics Of Everyday Life]]></name>
-
-     <description><![CDATA[Mama Buzz]]></description>  
-
-
-     <l>37.868146,-122.264678,0</l>
-     <o>0,0,0</o>
-
-   <minaccuracy/>
-          <maxdistance/>
-          <mime-type>image/png</mime-type>
-          <mainresource>http://hyperlocative.appspot.com/static/jasper_200.png</mainresource>
-
-     <thumbnail>http://hyperlocative.appspot.com/static/jasper_100.png</thumbnail>
-     <icon>http://hyperlocative.appspot.com/static/jasper_100.png</icon>
-</results>";
+    <results trackingurl=\"http://oaktopia.hyperlocative.com/resources/tracking.xml_enc\">
+       <poi id=\"1\" cosid=\"1\" interactionfeedback=\"none\">
+       <name><![CDATA[metaio Man]]></name>
+          <author><![CDATA[metaio]]></author>
+          <translation>0.0,0.0,0.0</translation>
+          <o>0.0,0.0,0.0</o>
+          <mime-type>model/md2</mime-type>
+          <mainresource><![CDATA[http://www.junaio.com/publisherDownload/tutorial/metaioman.md2_enc]]></mainresource>
+          <thumbnail>http://www.junaio.com/publisherDownload/tutorial/icon.jpg</thumbnail>
+          <icon>http://www.junaio.com/publisherDownload/tutorial/icon.jpg</icon>
+          <route>false</route>
+          <force3d>true</force3d>
+          <s>1</s>
+          <behaviours>
+             <behaviour type=\"click\"><length>6</length><node_id>close_up</node_id></behaviour>
+             <behaviour type=\"idle\"><length>0</length><node_id>idle</node_id></behaviour>
+          </behaviours>
+          <customizations/>
+          <resources><resource>http://www.junaio.com/publisherDownload/tutorial/metaioman.png</resource></resources>         
+       </poi>
+              <poi id=\"2\" cosid=\"2\" interactionfeedback=\"none\">
+              <name><![CDATA[TimeOut Video]]></name>
+                      <author><![CDATA[metaio]]></author>
+                      <o>0.0,0.0,0.0</o>
+                      <translation>0,0,0.0</translation>
+                      <minaccuracy>2</minaccuracy>
+                      <maxdistance>100000</maxdistance>
+                      <mime-type>model/md2</mime-type>
+                      <mainresource>http://www.junaio.com/publisherDownload/tutorial/movieplane.md2_enc</mainresource>
+                      <s>.3</s>
+                      <force3d>true</force3d>
+                      <resources>
+                             <resource>http://www.junaio.com/publisherDownload/tutorial/insideAR.3gp</resource> 
+                      </resources>      
+              </poi>
+    </results>";
 ?>
