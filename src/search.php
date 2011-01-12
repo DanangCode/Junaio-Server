ffcb8ab31df34f3645a5d0f7dff8ee9443c5051f
Jasper Gregory <jaspergregory@gmail.com>
2011-01-11 20:52:47 -0800

    .

--- src/search.php
+++ src/search.php
@@ -2,32 +2,23 @@
  
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
    <results>    
-      <poi id=\"poi1\" interactionfeedback=\"none\">
+      <poi id=\"poi1\" interactionfeedback=\"click\">
          <name><![CDATA[Hello 3D World]]></name>
          <description><![CDATA[This is my first 3D POI.]]></description>
          <author>YOU</author>
          <date/>
          <l>37.8048,-122.295427,0</l>
          <o>0,0,0</o>
-         <minaccuracy/>
-         <maxdistance/>
          <mime-type>model/md2</mime-type>
          <mainresource>http://oaktopia.hyperlocative.com/resources/cyper.md2</mainresource>
-         <force3d>true</force3d>
-         <s>.6</s>
-         <behaviours>
-            <behaviour type=\"idle\">
-               <!-- LENGTH: 0 for looping, amount of frames otherwise -->
-               <length>0</length>
-               <!-- NODE_ID: name of the animation in the 3D model -->
-               <node_id>frame</node_id>
-            </behaviour>
-         </behaviours>
+         <force3d>false</force3d>
+         <s>2</s>
+        
          <resources>
             <resource>http://oaktopia.hyperlocative.com/resources/cyber.png</resource>
          </resources>
-         <thumbnail>http://www.junaio.com/publisherDownload/tutorial/icon.jpg</thumbnail>
-         <icon>http://www.junaio.com/publisherDownload/tutorial/icon.jpg</icon>
+         <thumbnail>http://oaktopia.hyperlocative.com/resources/jasper_100.png</thumbnail>
+         <icon>http://oaktopia.hyperlocative.com/resources/jasper_100.png</icon>
          <homepage>http://www.metaio.com/</homepage>         
         </poi>
 </results>";
